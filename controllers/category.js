adb370cadb70f7ff44c08d77b45627d98e8c9ca5
NotHoudaifa <houdaifacherifi618@gmail.com>
2022-10-07 12:58:56 +0100

    added the feature to create a category

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,6 @@
 const Category = require('../models/category');
 const Item = require('../models/item');
+const { body, validationResult } = require('express-validator');
 
 // /category/:id
 exports.getCategory = function (req, res, next) {
@@ -17,22 +18,35 @@ exports.getCategory = function (req, res, next) {
 
 // /category/create
 exports.getCreateCategory = function (req, res, next) {
-    res.send(`GET Create Category`);
+    res.render('getCreateCategory', { title: 'Create Category' });
 };
 
 // /category/:id/update
 exports.getUpdateCategory = function (req, res, next) {
     res.send(`GET Update Category`);
 };
+
 // /category/:id/delete
 exports.getDeleteCategory = function (req, res, next) {
     res.send(`GET Delete Category`);
 };
 
 // /category/create
-exports.postCreateCategory = function (req, res, next) {
-    res.send(`POST Create Category`);
-};
+exports.postCreateCategory = [
+    body('name').isLength({ max: 15 }).notEmpty(),
+    body('description').isLength({ max: 50 }).escape(),
+    function (req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            next(new Error('An Error Happened'));
+        }
+        const category = new Category({ name: req.body.name, description: req.body.description || "" });
+        category.save().then((result) => {
+            console.log(result);
+            res.redirect(category.appUrl);
+        })
+    },
+];
 
 // /category/:id/update
 exports.postUpdateCategory = function (req, res, next) {
