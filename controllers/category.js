9a49f10ba985dc78eb98e921a9b38ae6a2db08e4
NotHoudaifa <houdaifacherifi618@gmail.com>
2022-10-07 21:41:34 +0100

    Finished All CRUD operations

--- controllers/category.js
+++ controllers/category.js
@@ -23,37 +23,74 @@ exports.getCreateCategory = function (req, res, next) {
 
 // /category/:id/update
 exports.getUpdateCategory = function (req, res, next) {
-    res.send(`GET Update Category`);
+    Category.findById(req.params.id).then((result) => {
+        res.render('getUpdateCategory', {
+            title: 'Update Category',
+            category: result,
+        });
+    });
 };
 
 // /category/:id/delete
 exports.getDeleteCategory = function (req, res, next) {
-    res.send(`GET Delete Category`);
+    Item.find({ category: req.params.id }).then((result) => {
+        console.log(result);
+        res.render('getDeleteCategory', {
+            title: 'Delete Category',
+            items: result,
+        });
+    });
 };
 
 // /category/create
 exports.postCreateCategory = [
-    body('name').isLength({ max: 15 }).notEmpty(),
+    body('name').isLength({ max: 15 }).notEmpty().exists(),
     body('description').isLength({ max: 50 }).escape(),
     function (req, res, next) {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             next(new Error('An Error Happened'));
         }
-        const category = new Category({ name: req.body.name, description: req.body.description || "" });
+        const category = new Category({
+            name: req.body.name,
+            description: req.body.description || '',
+        });
         category.save().then((result) => {
-            console.log(result);
             res.redirect(category.appUrl);
-        })
+        });
     },
 ];
 
 // /category/:id/update
-exports.postUpdateCategory = function (req, res, next) {
-    res.send(`POST Update Category`);
-};
+exports.postUpdateCategory = [
+    body('name').isLength({ max: 15 }).notEmpty().exists(),
+    body('description').isLength({ max: 50 }).escape(),
+    function (req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            next(new Error('An Error Happened'));
+        }
+        Category.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            description: req.body.description,
+        }).then((result) => {
+            res.redirect(result.appUrl);
+        });
+    },
+];
 
 // /category/:id/delete
 exports.postDeleteCategory = function (req, res, next) {
-    res.send(`POST Delete Category`);
+    Item.find({ category: req.params.id }).then((result) => {
+        if (result.length !== 0) {
+            next(
+                new Error(
+                    'You need to delete all books related to this category'
+                )
+            );
+        }
+        Category.findByIdAndDelete(req.params.id).then((result) => {
+            res.redirect('/');
+        });
+    });
 };
--- controllers/item.js
+++ controllers/item.js
@@ -1,40 +1,115 @@
 const Item = require('../models/item');
+const Category = require('../models/category');
+const { body, validationResult } = require('express-validator');
 
 // /item/:id
 exports.getItem = function (req, res, next) {
     Item.find({ _id: req.params.id })
-        .populate("category")
+        .populate('category')
         .exec()
         .then((result) => {
-            res.render("getItem", {title: "Item", item: result[0]})
+            res.render('getItem', { title: 'Item', item: result[0] });
         });
 };
 
 // /item/create
 exports.getCreateItem = function (req, res, next) {
-    res.send(`GET Create Item`);
+    Category.find({}).then((result) => {
+        res.render('getCreateItem', {
+            title: 'Create Item',
+            categorys: result,
+        });
+    });
 };
 
 // /item/:id/update
 exports.getUpdateItem = function (req, res, next) {
-    res.send(`GET Update Item`);
+    Promise.all([Item.findById(req.params.id), Category.find({})]).then(
+        (results) => {
+            console.log(results[0]._id);
+            res.render('getUpdateItem', {
+                title: 'Update Item',
+                item: results[0],
+                categorys: results[1],
+            });
+        }
+    );
 };
 // /item/:id/delete
 exports.getDeleteItem = function (req, res, next) {
-    res.send(`GET Delete Item`);
+    res.render("getDeleteItem", {title: "Delete Item"});
 };
 
 // /item/create
-exports.postCreateItem = function (req, res, next) {
-    res.send(`POST Create Item`);
-};
+exports.postCreateItem = [
+    body('name').exists().isLength({ min: 1, max: 50 }),
+    body('description').isLength({ max: 200 }),
+    body('category')
+        .exists()
+        .custom((value) => {
+            if (!Category.findById(value).count()) {
+                throw new Error('Category does not exist');
+            }
+            return true;
+        }),
+    body('author').exists().isLength({ min: 1, max: 30 }),
+    body('url').exists().isLength({ min: 1 }),
+    function (req, res, next) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            next(new Error('Invalid Data'));
+        }
+
+        const item = new Item({
+            name: req.body.name,
+            description: req.body.description || '',
+            category: req.body.category,
+            author: req.body.author,
+            url: req.body.url,
+        });
+        item.save().then((result) => {
+            res.redirect(result.appUrl);
+        });
+    },
+];
 
 // /item/:id/update
-exports.postUpdateItem = function (req, res, next) {
-    res.send(`POST Update Item`);
-};
+exports.postUpdateItem = [
+    body('name').exists().isLength({ min: 1, max: 50 }),
+    body('description').isLength({ max: 200 }),
+    body('category')
+        .exists()
+        .custom((value) => {
+            if (!Category.findById(value).count()) {
+                throw new Error('Category does not exist');
+            }
+            return true;
+        }),
+    body('author').exists().isLength({ min: 1, max: 30 }),
+    body('url').exists().isLength({ min: 1 }),
+    function (req, res, next) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            next(new Error('Invalid Data'));
+        }
+
+        Item.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            author: req.body.author,
+            url: req.body.url,
+        }).then((result) => {
+            res.redirect(result.appUrl);
+        });
+    },
+];
 
 // /item/:id/delete
 exports.postDeleteItem = function (req, res, next) {
-    res.send(`POST Delete Item`);
+    Item.findByIdAndDelete(req.params.id).then((result) => {
+        res.redirect("/")
+    })
 };
